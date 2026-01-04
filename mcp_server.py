4b7c9eed8f6260f2a6963338967b45d085130552
Omar-Saeed-OASS <omarabdulrahmansaeed@gmail.com>
2026-01-04 12:49:02 +0800

    Initial setup of MCP Server

---
+++ mcp_server.py
@@ -0,0 +1,51 @@
+from mcp.server.fastmcp import FastMCP
+import requests
+import json
+
+# Server intialization
+mcp = FastMCP("APSpace")
+
+# Tools
+@mcp.tool()
+def get_student_timetable(intake_code: str) -> str:
+    """Retrieves the weekly timetable for a specific intake code."""
+    
+    timetable_url = "https://s3-ap-southeast-1.amazonaws.com/open-ws/weektimetable"
+
+    try:
+        response = requests.get(timetable_url)
+        response.raise_for_status()
+        all_timetable = response.json()
+
+        # filter the timetable
+        filtered_timetable = [
+            x for x in all_timetable 
+            if x.get("INTAKE_CODE") == intake_code
+        ]
+
+        if not filtered_timetable:
+            return f"No classes found for intake: {intake_code}."
+
+        # Format the filtered timetable
+        formatted_schedule = []
+        for module in filtered_timetable:
+            formatted_schedule.append(
+                {
+                    "module": module.get("MODID"),
+                    "group": module.get("GROUPING"),
+                    "day": module.get("DAY"),
+                    "date": module.get("DATESTAMP"),
+                    "from": module.get("TIME_FROM"),
+                    "to": module.get("TIME_TO"),
+                    "location": module.get("LOCATION"),
+                    "lecturer": module.get("SAMACCOUNTNAME")
+                }
+            )
+        
+        return json.dumps(formatted_schedule[:20]) 
+
+    except Exception as e:
+        return f"Error fetching timetable: {str(e)}"
+
+if __name__ == "__main__":
+    mcp.run()
